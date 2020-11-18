0808c0ab7aace39414c443b8ad9ebd86e8c4b241
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-11-17 17:07:55 -0700

    Added the ability to create new racks with validation and error handling

--- controllers/rackController.js
+++ controllers/rackController.js
@@ -1,4 +1,6 @@
 var Rack = require('../models/rack');
+var Brand = require('../models/brand');
+const {body, validationResult } = require('express-validator');
 
 exports.rack_list = function(req, res){
     Rack.find({}, 'type brand price')
@@ -19,12 +21,57 @@ exports.rack_detail = function(req, res, next){
 }
 
 exports.rack_create_get = function(req, res){
-    res.send('NOT IMPLEMENTED: rack create GET');
+    Brand.find()
+    .exec(function(err, brands){
+        if(err)return next(err);
+        res.render('rack_form',{title:'Create Rack', brands:brands});
+    });
 }
 
-exports.rack_create_post = function(req, res){
-    res.send('NOT IMPLEMENTED: rack create POST');
-}
+exports.rack_create_post = [
+    function(req, res, next) {
+        if(req.body.brand=='') req.body.brand=null;
+        next();
+    },
+
+    body('type', 'Rack type required').trim().isLength({min:1}).escape(),
+    body('brand').optional({checkFalsy:true}).escape(),
+    body('price', 'Rack price required').trim().isFloat({min:0}).escape(),
+    body('stock', 'Stock required').trim().isFloat({min:0}).escape(),
+    body('gauge', 'Rack gauge required').trim().isFloat({min:0}).escape(),
+    body('profile', 'Rack profile required').trim().isLength({min:1}).escape(),
+    body('capacity').optional({checkFalsy:true}).trim().isFloat({min:0}).escape(),
+    body('unit', 'Weight units required').trim().escape(),
+    body('safeties', 'Safeties type required').trim().isLength({min:1}).escape(),
+
+    (req,res,next)=>{
+        const errors = validationResult(req);
+        var rack = new Rack({
+            type:req.body.type,
+            brand:req.body.brand,
+            price:req.body.price,
+            stock:req.body.stock,
+            gauge:req.body.gauge,
+            profile:req.body.profile,
+            capacity:req.body.capacity,
+            unit:req.body.unit,
+            safeties:req.body.safeties,
+        });
+
+        if(!errors.isEmpty()){
+            Brand.find()
+            .exec(function(err,brands){
+                if(err) return next (err);
+                res.render('rack_form',{title:'Create Rack', errors:errors.array(), brands:brands, rack:rack});
+            });
+        }else{
+            rack.save(function(err){
+                if(err) return next(err);
+                res.redirect(rack.url);
+            })
+        }
+    }
+]
 
 exports.rack_delete_get = function(req, res){
     res.send('NOT IMPLEMENTED: rack delete GET');
